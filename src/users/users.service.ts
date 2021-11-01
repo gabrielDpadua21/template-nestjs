261ee5d0be5b9a50993cbda9021dc75c479b79cd
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-01 02:14:02 -0300

    ✨feat(project): user service create user and create user admin

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserRole } from './enums/users-role.enum';
+import { User } from './users.entity';
+import { UserRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password !== createUserDto.passwordConfirm)
+      throw new UnprocessableEntityException('password is not equal');
+    return this.userRepository.createUser(createUserDto, UserRole.USER);
+  }
+
+  async createAdmin(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password !== createUserDto.passwordConfirm)
+      throw new UnprocessableEntityException('password is not equal');
+    return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+  }
+}
