32d7dd68c5f7af4b7c7f65b1ba5c00aa61b43269
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-01 02:15:10 -0300

    ✨feat(project): user controller post create user and create user admin

---
+++ src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ValidationTypes } from 'class-validator';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { ResponseDto } from './dtos/response-dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  async create(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ResponseDto> {
+    const user = await this.userService.create(createUserDto);
+    return new ResponseDto(user, 'Successs');
+  }
+
+  @Post('/admin')
+  async createAdmin(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ResponseDto> {
+    const user = await this.userService.createAdmin(createUserDto);
+    return new ResponseDto(user, 'Successs');
+  }
+}
