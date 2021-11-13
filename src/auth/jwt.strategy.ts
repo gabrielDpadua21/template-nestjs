0c778f58fa6929389eb791be1eaf842804c08899
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-13 04:55:57 -0300

    ✨feat(file): create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../users/users.repository';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { id: number }) {
+    const { id } = payload;
+    const user = await this.userRepository.findOne(id, {
+      select: ['name', 'email', 'status', 'role'],
+    });
+    if (!user) throw new UnauthorizedException('User not found');
+    return user;
+  }
+}
