d7606d2b99a680fdf2af4bb3db9304e48c008056
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-13 04:58:24 -0300

    🧪test(file): testing /me to verify guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/users.entity';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
 import { AuthService } from './auth.service';
 import { CredentialsDto } from './dtos/credentials.dto';
 import { ResponseSignDto } from './dtos/response-sign.dto';
+import { GetUser } from './get-user.docorator';
 
 @Controller('auth')
 export class AuthController {
@@ -23,5 +33,11 @@ export class AuthController {
     @Body(ValidationPipe) credentialsDto: CredentialsDto,
   ): Promise<ResponseSignDto> {
     return await this.authService.signIn(credentialsDto);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  getMe(@GetUser() user: User): User {
+    return user;
   }
 }
