b43c1b9660d02d50cde699f435437b3f0b033798
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-13 04:57:52 -0300

    🔖chore(file): update imports auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { UserRepository } from 'src/users/users.repository';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
@@ -17,7 +20,8 @@ require('dotenv').config();
       },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
